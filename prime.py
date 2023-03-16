711390e82a904afcac2e5125f3771f9a9726ff9a
VarrinkalaPrasad <90542933+VarrinkalaPrasad@users.noreply.github.com>
2023-03-16 23:27:44 +0530

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,18 @@
+# program to check if a number is prime or not
+
+num = int(input("enter the number: "))
+
+if num ==1:
+    print(num, "is not a prime number")
+elif num > 1:
+    for i in range(2,num):
+        if (num%i) ==0:
+            print(num, "is not a prime number")
+            print(i,"times",num//i,"is",num)
+            break
+            
+    else:
+        print(num, "is a prime number")
+        
+else:
+    print(num,"is not a prime")
